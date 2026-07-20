17e90a52781f959afa483f33e41b67c8dc554b60
Your Name <your@email.com>
2026-07-20 23:52:57 +0100

    Fix unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,9 @@ import {
 import { useEffect } from 'react';
 
 export default function Home() {
+   useEffect(() => {
+    document.title = "JpDEV.STUDIO";
+  }, []);
   return (
     <div className="max-w-[1360px] mx-auto p-4 md:p-8">
       <div className="grid grid-cols-1 md:grid-cols-12 gap-6 auto-rows-auto">
